9ce5e1649bbf181fb2a50dd8339f21772318e483
Abirami <103939322+Abiramirajasekar@users.noreply.github.com>
2023-08-03 15:29:11 +0530

    new

--- Backend_/server.js
+++ Backend_/server.js
@@ -3,6 +3,21 @@ const express = require("express");
 const mongoose =  require("mongoose");
 const routes = require("./router/auth.route");
 
+
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+
 const app = express()
 app.use(express.json())
 app.use("/api",routes)
