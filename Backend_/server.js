149d1edfcb8d73f16d30c2db1667a1823369ebd9
Abirami <103939322+Abiramirajasekar@users.noreply.github.com>
2023-08-03 16:16:11 +0530

    Add files via upload

--- Backend_/server.js
+++ Backend_/server.js
@@ -4,18 +4,6 @@ const mongoose =  require("mongoose");
 const routes = require("./router/auth.route");
 
 
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
-
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
-
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
 
 
 const app = express()
