6643ee0b395e21ccb9b99972e0f8c6fe04ef4f06
Abirami <103939322+Abiramirajasekar@users.noreply.github.com>
2023-08-04 11:34:38 +0530

    Add files via upload

---
+++ Backend_/src/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express = require("express");
+const mongoose =  require("mongoose");
+const routes = require("../router/auth.route");
+
+
+
+
+const app = express()
+app.use(express.json())
+app.use("/api",routes)
+
+// Connect to mongodb
+mongoose.connect(process.env.DB_CONNECTION_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+})
+const database = mongoose.connection
+database.on("error",(err)=> console.log("ERROR:",err))
+database.on("connected",()=>console.log("Database Connected"))
+
+
+// app.get("/",(req,res=>{
+//     res.send("Welcome!")})
+
+app.listen(3000,()=>{
+console.log("Successfully your server started!")
+})
